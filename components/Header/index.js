3a56ddd1221f8f9a80c4faa6753bacb65ab50b56
Melanie C <melanie.chedhomme96@gmail.com>
2024-01-02 16:09:36 +0100

    neutral menu mobile

--- components/Header/index.js
+++ components/Header/index.js
@@ -64,7 +64,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
             </div>
             <Popover.Panel
               className={`absolute right-0 z-10 w-11/12 p-4 ${
-                theme === "dark" ? "bg-slate-800" : "bg-white"
+                theme === "dark" ? "bg-neutral-800" : "bg-white"
               } shadow-md rounded-md`}
             >
               {!isBlog ? (
