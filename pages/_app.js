179d5ca7d7d82902ce3472f738b5b3c12bd98695
Melanie C <melanie.chedhomme96@gmail.com>
2024-01-03 20:40:42 +0100

    essai style dans theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-import "../styles/globals.css";
+// pages/_app.js
 import { ThemeProvider } from "next-themes";
+import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
+      <style jsx global>{`
+        html {
+          font-family: 'Quicksand', sans-serif; 
+        }
+      `}</style>
       <Component {...pageProps} />
     </ThemeProvider>
   );
