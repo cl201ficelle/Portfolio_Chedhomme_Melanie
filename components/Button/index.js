74d02950251ebd002c30b413bd56c7c6b32649a7
Melanie C <melanie.chedhomme96@gmail.com>
2024-01-04 12:16:05 +0100

    button size

--- components/Button/index.js
+++ components/Button/index.js
@@ -5,28 +5,28 @@ import data from "../../data/portfolio.json";
 const Button = ({ children, type, onClick, classes }) => {
   // thème actuel
   const { theme } = useTheme();
-  // bouton type primary
-  if (type === "primary") {
-    return (
-      <button
-        onClick={onClick}
-        type="button"
-        className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg ${
-          theme === "dark" ? "bg-white text-black" : "bg-black text-white"
-        }  transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100 link ${
-          data.showCursor && "cursor-none"
-        }  ${classes}`}
-      >
-        {children}
-      </button>
-    );
-  }
+  // // bouton type primary
+  // if (type === "primary") {
+  //   return (
+  //     <button
+  //       onClick={onClick}
+  //       type="button"
+  //       className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg ${
+  //         theme === "dark" ? "bg-white text-black" : "bg-black text-white"
+  //       }  transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100 link ${
+  //         data.showCursor && "cursor-none"
+  //       }  ${classes}`}
+  //     >
+  //       {children}
+  //     </button>
+  //   );
+  // }
   // code pour autre bouton
   return (
     <button
       onClick={onClick}
       type="button"
-      className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg flex items-center transition-all ease-out duration-300 ${
+      className={`laptop:text-lg tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg flex items-center transition-all ease-out duration-300 ${
         theme === "dark"
           ? "hover:bg-zinc-600 text-white"
           : "hover:bg-pink-100"
