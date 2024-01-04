3511961eaa191cb5781500025cd84f0e252cf1a9
Melanie C <melanie.chedhomme96@gmail.com>
2024-01-04 12:31:44 +0100

    supp bouton et gradient shadow

--- components/Button/index.js
+++ components/Button/index.js
@@ -5,23 +5,6 @@ import data from "../../data/portfolio.json";
 const Button = ({ children, type, onClick, classes }) => {
   // thème actuel
   const { theme } = useTheme();
-  // // bouton type primary
-  // if (type === "primary") {
-  //   return (
-  //     <button
-  //       onClick={onClick}
-  //       type="button"
-  //       className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg ${
-  //         theme === "dark" ? "bg-white text-black" : "bg-black text-white"
-  //       }  transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100 link ${
-  //         data.showCursor && "cursor-none"
-  //       }  ${classes}`}
-  //     >
-  //       {children}
-  //     </button>
-  //   );
-  // }
-  // code pour autre bouton
   return (
     <button
       onClick={onClick}
