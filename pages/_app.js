f71dae6929950840aacbb6c180fcf83828908e08
Melanie C <melanie.chedhomme96@gmail.com>
2024-01-05 08:54:11 +0100

    try font

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import {Quicksand} from '@next/font/google'
-// const quicksand = Quicksand ({
-//   subsets:['latin'],
-//   weights: ['300','400','500','600','700']
-// })
+
 // application 
 const App = ({ Component, pageProps }) => {
   return (
