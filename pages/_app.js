6efa47db738c95ee330fa51bc1b444a493acecb4
Melanie C <melanie.chedhomme96@gmail.com>
2024-01-05 08:51:09 +0100

    try

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,17 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import {Quicksand} from '@next/font/google'
-const quicksand = Quicksand ({
-  subsets:['latin'],
-  weight: ['300','400','500','600','700']
-})
+// const quicksand = Quicksand ({
+//   subsets:['latin'],
+//   weights: ['300','400','500','600','700']
+// })
 // application 
 const App = ({ Component, pageProps }) => {
   return (
-    <main className={quicksand.className}>
     <ThemeProvider>
       <Component {...pageProps} />
     </ThemeProvider>
-    </main>
+    
   );
 };
 
